a4f5f9a253bc8ff915d31e9c22809f61eeb5861d
Evgeniy Kuzmin <evgenii.bad@gmail.com>
2020-01-31 08:21:36 +0400

    1) Now client send a json request, and we easy parse it and recieve a Json object to have more simple CRUD operation with BD.
    2)Re-work logic because changes in point 1)

--- src/main/java/my/project/servlet/test/model/domain/Worker.java
+++ src/main/java/my/project/servlet/test/model/domain/Worker.java
@@ -5,7 +5,10 @@ import org.json.simple.JSONObject;
 public class Worker {
 
     int id;
+    int salary;
     String surname;
+    String position;
+
 
     public int getId() {
         return id;
@@ -23,10 +26,28 @@ public class Worker {
         this.surname = surname;
     }
 
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
     public JSONObject toJson() {
         JSONObject obj = new JSONObject();
         obj.put("id", id);
         obj.put("surname", surname);
+        obj.put("salary", salary);
+        obj.put("position", position);
         return obj;
     }
 }
